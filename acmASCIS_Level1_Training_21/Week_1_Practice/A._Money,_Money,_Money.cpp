938100ebe792b444cc2ca374f6db28c9a787cd8d
YM3 <ym3@gmail>
2023-08-10 01:26:38 +0300

    tiread

---
+++ acmASCIS_Level1_Training_21/Week_1_Practice/A._Money,_Money,_Money.cpp
@@ -0,0 +1,17 @@
+// unsolved 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n , x , y ;
+    cin>> n >> x >> y;
+    int a = y-x ;
+    int b = n * a ;
+
+
+    cout<<b<<endl;
+    return 0;
+}
+
